e68a6ccdd1289f0f99d9ab1b3b168fe8d8948e2f
Theorhetically <56592882+Theorhetically@users.noreply.github.com>
2021-11-25 16:21:46 +0800

    Add files via upload

--- Witch2DEnglishVocab/Assets/Player_Script/PlayerMovement.cs
+++ Witch2DEnglishVocab/Assets/Player_Script/PlayerMovement.cs
@@ -9,10 +9,13 @@ public class PlayerMovement : MonoBehaviour {
 
 	float horzMove = 0f;
 	bool jump = false;
+    public Animator animator;
 
 	void Update() {
 		
 		horzMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horzMove));
 
 		if(Input.GetButtonDown("Jump")) {
 			jump = true;
